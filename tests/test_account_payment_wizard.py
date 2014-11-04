47a22fd5d0145327f8523091eaf42a12893e81b5
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 13:18:42 +0100

    Migrate to version 3.4

--- tests/test_account_payment_wizard.py
+++ tests/test_account_payment_wizard.py
@@ -1,49 +1,24 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
-from trytond.backend.sqlite.database import Database as SQLiteDatabase
 
 
 class AccountPaymentWizardCase(unittest.TestCase):
-    '''
-    Account Payment Wizard module.
-    '''
+    'Account Payment Wizard module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('account_payment_wizard')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('account_payment_wizard')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
-
-
-def doctest_dropdb(test):
-    database = SQLiteDatabase().connect()
-    cursor = database.cursor(autocommit=True)
-    try:
-        database.drop(cursor, ':memory:')
-        cursor.commit()
-    finally:
-        cursor.close()
 
 
 def suite():
@@ -51,6 +26,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountPaymentWizardCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
