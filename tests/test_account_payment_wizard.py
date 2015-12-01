e742c2bbf373fe0204a95310378a39351810e993
resteve <resteve@zikzakmedia.com>
2015-12-01 11:02:10 +0100

    Tests

--- tests/test_account_payment_wizard.py
+++ tests/test_account_payment_wizard.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the account_payment_wizard module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountPaymentWizardCase(unittest.TestCase):
-    'Account Payment Wizard module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_payment_wizard')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_payment_wizard')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountPaymentWizardTestCase(ModuleTestCase):
+    'Test Account Payment Wizard module'
+    module = 'account_payment_wizard'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountPaymentWizardCase))
+        AccountPaymentWizardTestCase))
     return suite
